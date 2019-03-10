3940e6bf0463f5bfc9b0de1a2c36d915bdf6d68e
Selçuk Karakayalı <skarakayali@gmail.com>
2019-03-10 22:37:37 +0300

    polish

--- whoisit/__init__.py
+++ whoisit/__init__.py
@@ -1,11 +1,5 @@
 import logging
-print("hey")
-logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
-print("hey1")
 
-#from . import config
 from .app import main
-print("hey3")
-#from . import authn
-#from . import twt
-#from . import utils
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
