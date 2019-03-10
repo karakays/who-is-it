b8fa30f1b5b145520ce93112cb4eeb522aaa243b
Selçuk Karakayalı <skarakayali@gmail.com>
2019-03-10 22:36:36 +0300

    version 0.2 alpha' ' '

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='who-is-it',
-    version='0.1-dev',
+    version='0.2a1.dev',
     author='Selçuk Karakayalı',
     author_email='skarakayali@gmail.com',
     maintainer='Selçuk Karakayalı',
