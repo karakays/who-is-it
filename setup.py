d078499051cd27069d835eb69d52d409b97ed4cb
Selçuk Karakayalı <skarakayali@gmail.com>
2019-03-11 15:50:06 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,23 @@
 # -*- coding: utf-8
 from setuptools import setup, find_packages
 
+pkg_vars = {}
+
+with open("whoisit/_version.py") as fp:
+    exec(fp.read(), pkg_vars)
+
 setup(
-    name='who-is-it',
-    version='0.2a1.dev',
+    name='whoisit',
+    version=pkg_vars['__version__'],
     author='Selçuk Karakayalı',
     author_email='skarakayali@gmail.com',
-    maintainer='Selçuk Karakayalı',
-    url='http://github.com/karakays/who-is-it',
+    url='https://github.com/karakays/who-is-it',
+    description='Find out who has unfollowed you on Twitter',
+    install_requires=['requests>=2.21.0'],
+    license='MIT',
     packages=find_packages(),
     python_requires='>=3',
-    license='MIT',
-    keywords=['otp', 'otp-codes', 'otp-generator', 'authenticator'],
-    description='Generate one time passwords',
+    keywords=['twitter', 'followers', 'authentication'],
     long_description=open('README.rst').read(),
     scripts=['bin/whoisit']
 )
